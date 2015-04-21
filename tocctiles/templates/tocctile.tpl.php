6d6297529ea87534d45b848a8dd222f49c724846
Ben Hall <b.hall@tocc.co.uk>
2015-04-21 17:00:10 +0100

    End of 21st April

---
+++ tocctiles/templates/tocctile.tpl.php
@@ -0,0 +1,2 @@
+<img src="<?php print $path; ?>"> 
+        
--- tocctiles/templates/tocctiles_block.tpl.php
+++ tocctiles/templates/tocctiles_block.tpl.php
@@ -1,6 +1,1 @@
-<section id="tocc_tiles-gallery-pane">
-    <nav class="image-categories"></nav>
-    <div class="gallery">
-        <div class="gallery-inner"></div>
-    </div>
-</section>
+<?php echo '<div> class="tiles-block"></div>';
