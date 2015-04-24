e46608a21a7ec005f2042566c9ca76db8bf18bec
Ben Hall <b.hall@tocc.co.uk>
2015-04-24 10:04:34 +0100

    Functioning module and ready to be integrated into the responsive repo

--- tocctiles/templates/tocctile.tpl.php
+++ tocctiles/templates/tocctile.tpl.php
@@ -1,10 +1,8 @@
 <?php
-/*
-echo $image;
 
-<h3>
-    <a>
-        Text
-        <span>button</span>
-        <img>
- */       
+echo sprintf('<div class="tocc-tile">' .
+                    '<h3><a href="%s" target="_self">%s%s</h3></a>' .
+                        '<span class="tocc-tile__desc">%s</span>' .
+                        '<span class="tocc-tile__button">%s</span></div>',
+                        $link, $image, $name, $description, $buttontext);
+
